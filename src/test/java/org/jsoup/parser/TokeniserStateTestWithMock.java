6beb2a12cd8a658889625f2115488b21bad0f2b4
ch-zha <chizhang95@gmail.com>
2020-02-09 19:26:13 -0800

    10 fsm tests

--- src/test/java/org/jsoup/parser/TokeniserStateTestWithMock.java
+++ src/test/java/org/jsoup/parser/TokeniserStateTestWithMock.java
@@ -17,6 +17,12 @@ public class TokeniserStateTestWithMock {
         return spy(tokeniser);
     }
 
+    public Tokeniser setup(String input, TokeniserState state) {
+        Tokeniser tokeniser = new Tokeniser(new CharacterReader(input), ParseErrorList.noTracking());
+        tokeniser.transition(state);
+        return spy(tokeniser);
+    }
+
     @Test
     public void testTokeniser() {
         Tokeniser tokeniser = setup("<div>");
@@ -28,9 +34,126 @@ public class TokeniserStateTestWithMock {
 
     @Test
     public void testTokeniserTagOpen() {
-        Tokeniser tokeniser = setup("div>");
-        tokeniser.transition(TokeniserState.TagOpen);
+        Tokeniser tokeniser = setup("div>", TokeniserState.TagOpen);
         tokeniser.read();
-        verify(tokeniser, times(1)).transition(TokeniserState.TagOpen);
+        verify(tokeniser, times(1)).transition(TokeniserState.TagName);
+    }
+
+    @Test
+    public void testScriptDataEndTag() {
+        Tokeniser tokeniser = setup("</div>", TokeniserState.ScriptData);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.read();
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataLessthanSign);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataEndTagOpen);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataEndTagName);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptData);
+    }
+
+    @Test
+    public void testScriptDataEscapeStart() {
+        Tokeniser tokeniser = setup("--<abc>", TokeniserState.ScriptDataEscapeStart);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.read();
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataEscapeStartDash);
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataEscapedDashDash);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataEscapedLessthanSign);
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataDoubleEscapeStart);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataEscaped);
+        inOrder.verify(tokeniser).transition(TokeniserState.Data);
+    }
+
+    @Test
+    public void testScriptDataEscaped() {
+        Tokeniser tokeniser = setup("-</div>", TokeniserState.ScriptDataEscaped);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.read();
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataEscapedDash);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataEscapedLessthanSign);
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataEscapedEndTagOpen);
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataEscapedEndTagName);
+        inOrder.verify(tokeniser).transition(TokeniserState.Data);
+    }
+
+    @Test
+    public void testScriptDataDoubleEscaped() {
+        Tokeniser tokeniser = setup("--</", TokeniserState.ScriptDataDoubleEscaped);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.read();
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataDoubleEscapedDash);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataDoubleEscapedDashDash);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataDoubleEscapedLessthanSign);
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataDoubleEscapeEnd);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataDoubleEscaped);
+        inOrder.verify(tokeniser).transition(TokeniserState.Data);
+    }
+
+    @Test
+    public void testScriptDataDoubleEscapedDash() {
+        Tokeniser tokeniser = setup("-<a", TokeniserState.ScriptDataDoubleEscaped);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.read();
+        inOrder.verify(tokeniser).advanceTransition(TokeniserState.ScriptDataDoubleEscapedDash);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataDoubleEscapedLessthanSign);
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptDataDoubleEscaped);
+        inOrder.verify(tokeniser).transition(TokeniserState.Data);
+    }
+
+    @Test
+    public void testScriptDataEscapedDashDash() {
+        Tokeniser tokeniser = setup(">", TokeniserState.ScriptDataEscapedDashDash);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.read();
+        inOrder.verify(tokeniser).transition(TokeniserState.ScriptData);
+    }
+
+    @Test
+    public void testAttributeName() {
+        Tokeniser tokeniser = setup("id = \"hello\" /", TokeniserState.BeforeAttributeName);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.createTagPending(true);
+        tokeniser.read();
+        inOrder.verify(tokeniser).transition(TokeniserState.AttributeName);
+        inOrder.verify(tokeniser).transition(TokeniserState.AfterAttributeName);
+        inOrder.verify(tokeniser).transition(TokeniserState.BeforeAttributeValue);
+        inOrder.verify(tokeniser).transition(TokeniserState.AttributeValue_doubleQuoted);
+        inOrder.verify(tokeniser).transition(TokeniserState.AfterAttributeValue_quoted);
+        inOrder.verify(tokeniser).transition(TokeniserState.SelfClosingStartTag);
+        inOrder.verify(tokeniser).transition(TokeniserState.Data);
+    }
+
+    @Test
+    public void testAttributeValueUnquotedSingleQuoted() {
+        Tokeniser tokeniser = setup("hello color=\'blue\'>", TokeniserState.BeforeAttributeValue);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.createTagPending(true);
+        tokeniser.read();
+        inOrder.verify(tokeniser).transition(TokeniserState.AttributeValue_unquoted);
+        inOrder.verify(tokeniser).transition(TokeniserState.BeforeAttributeName);
+        /* Some untested transitions here */
+        inOrder.verify(tokeniser).transition(TokeniserState.BeforeAttributeValue);
+        inOrder.verify(tokeniser).transition(TokeniserState.AttributeValue_singleQuoted);
+        inOrder.verify(tokeniser).transition(TokeniserState.AfterAttributeValue_quoted);
+        inOrder.verify(tokeniser).transition(TokeniserState.Data);
+        System.out.println(tokeniser.getState());
+    }
+
+    @Test
+    public void testBeforeAttributeName() {
+        Tokeniser tokeniser = setup("color /", TokeniserState.AfterAttributeValue_quoted);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.createTagPending(true);
+        tokeniser.read();
+        inOrder.verify(tokeniser).transition(TokeniserState.BeforeAttributeName);
+        inOrder.verify(tokeniser).transition(TokeniserState.SelfClosingStartTag);
+    }
+
+    @Test
+    public void testAfterAttributeName() {
+        Tokeniser tokeniser = setup(" color", TokeniserState.AfterAttributeName);
+        InOrder inOrder = Mockito.inOrder(tokeniser);
+        tokeniser.createTagPending(true);
+        tokeniser.read();
+        inOrder.verify(tokeniser).transition(TokeniserState.AttributeName);
     }
 }
