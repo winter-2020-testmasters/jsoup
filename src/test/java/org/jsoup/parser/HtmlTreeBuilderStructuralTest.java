4996eecf17000961b9690e8d80fbd88afd284feb
ch-zha <chizhang95@gmail.com>
2020-02-16 12:47:36 -0800

    added new test class for structural testing

---
+++ src/test/java/org/jsoup/parser/HtmlTreeBuilderStructuralTest.java
@@ -0,0 +1,12 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+
+public class HtmlTreeBuilderStructuralTest {
+
+    @Test
+    public void newTest() {
+        System.out.println("Hello, World");
+    }
+
+}
